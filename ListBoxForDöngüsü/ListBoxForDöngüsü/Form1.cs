23a15a7913d17d40736aa1629217efcafb2dc177
HuseyinUzun <118770011+HuseyinUzun@users.noreply.github.com>
2023-02-10 22:19:34 +0300

    Add files via upload

---
+++ ListBoxForDöngüsü/ListBoxForDöngüsü/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxForDöngüsü
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*bu proje ile textboxa girilen sayının,2 ile bölümünden kalanı 0 olan yani
+       çift sayıları list boxa yazdırmssını sağladık*/
+            //listBox1.Items.Clear();
+            //int sayi = Convert.ToInt32(textBox1.Text);
+
+
+            /*Faktöriyel Hesaplama Programı*/
+
+            listBox1.Items.Clear();
+            int sonuc = 1;
+            int sayi = Convert.ToInt32(textBox1.Text);
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                sonuc = sonuc * i;
+                listBox1.Items.Add(sonuc);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int sayi = Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i <= sayi; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+
+            }
+        }
+    }
+}
---
+++ Login Uygulaması/Login Uygulaması/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login_Uygulaması
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hakSayisi = 3;
+
+            while (true)
+            {
+                Console.WriteLine("Kullanıcı adınızı giriniz");
+                string kullanıcıAdi = Console.ReadLine();
+
+                Console.WriteLine("Şifrenizi giriniz");
+                string sifre = Console.ReadLine();
+
+                if (kullanıcıAdi == "hüseyin" && sifre == "123")
+                {
+                    Console.WriteLine("Tebrikler başarılı bir şekilde giriş yaptınız :)");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adınız veya şifreniz hatalı");
+
+                    if (hakSayisi > 0)
+                    {
+                        hakSayisi -= 1;
+
+                    }
+                    if (hakSayisi == 0)
+                    {
+                        Console.WriteLine("3 defa hatalı girişten dolayı hesabınız blokelendi");
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
